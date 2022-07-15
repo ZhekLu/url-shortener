25aa490a91b38839975c4b59d91a50281bcdc4f4
ZhekLu <justletmego@list.ru>
2022-07-15 23:46:46 +0300

    Admin panel

--- simplify_app/admin.py
+++ simplify_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from simplify_app.models import User, SimpleUrl
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'is_activated', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    fields = (
+        ('username', 'email'),
+        ('first_name', 'last_name'),
+        ('send_messages', 'is_active', 'is_activated'),
+        ('is_staff', 'is_superuser'),
+        'groups', 'user_permissions',
+        ('last_login', 'date_joined')
+    )
+    readonly_fields = ('last_login', 'date_joined')
+
+
+admin.site.register(User, UserAdmin)
+
+
+class SimpleUrlAdmin(admin.ModelAdmin):
+    list_display = ('simple_url_id', 'original_url', 'user', 'created_at')
+
+
+admin.site.register(SimpleUrl, SimpleUrlAdmin)
